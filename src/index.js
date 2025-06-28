e9c2fc91068275b2da3a0784bbca4dd8bd6393c7
10Pratik01 <using0this@gmail.com>
2025-06-28 23:39:54 +0530

    Setting up CORS

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+dotenv.config({
+    path:"./env"
+})
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`SERVER IS RUNNING AT PORT: ${process.env.PORT}`)
+    })
+    
+    app.on("error", (error)=>{
+        console.log("There is an ERROR:" , error)
+    })
+})
+.catch((error)=>{
+    console.error("DATABASE CONNECTION ERROR", error);
+})
+
