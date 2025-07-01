44d598cab2c01079c8832092c53453fdedd3e089
10Pratik01 <using0this@gmail.com>
2025-07-02 03:21:20 +0530

    installed jwt bcrypt and mongooseAggregation

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
 
 const userSchema = new mongoose.Schema(
     {
@@ -50,5 +53,43 @@ const userSchema = new mongoose.Schema(
         timestamps: true
     }
 )
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next()
+    
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+});
+
+userSchema.methods.isPasswordCorrect = async function(pass){
+    return await bcrypt.compare(pass, this.password)
+};
+
+userSchema.methods.generateAccessToken = async function(token){
+    return jwt.sign(
+        {
+            _id: this._id, 
+            userName: this.userName, 
+            fullName: this.fullName, 
+            email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        } 
+    )
+}
+
+userSchema.methods.generateRefreshToken = async function(token){
+    return jwt.sign(
+        {
+            _id : this._id, 
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY, 
+        }
+    )
+}
 
 export  const User = mongoose.model("User", userSchema)
