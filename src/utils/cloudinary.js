6afe815268581da8f3d222485da5d031ea7b9e56
10Pratik01 <using0this@gmail.com>
2025-07-03 21:02:29 +0530

    Started controllers

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+
+    // Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_API_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async function (localUrl){
+    try {
+        if(!localUrl) return null
+        // to upload the file on cloudinary 
+        const response = await cloudinary.uploader.upload(localUrl, {
+            resource_type: "auto", 
+        })
+        console.log("File is uploaded successfully on cloudinary", response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localUrl)
+        return null 
+    }
+}
+    
+
+export default uploadOnCloudinary; 
